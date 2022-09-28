272fb6f79d8ed09055b89bc06ca06a331bb29d68
uliana1302 <uvvorobyova@edu.hse.ru>
2022-09-28 15:55:25 +0300

    28.09

---
+++ js/images.js
@@ -0,0 +1,8 @@
+const img_style = document.querySelectorAll("img")[1]
+img_style.style = "background-color: yellow; padding: 15px; border: 3px black solid; margin: 35px"
+
+const tagName_p = document.getElementsByTagName("p")[1]
+tagName_p.style.backgroundColor = "grey"
+
+const idName_id = document.getElementById("id_bg")
+idName_id.style = "background-color: yellow; padding: 15px; border: 3px black solid; margin: 35px"
